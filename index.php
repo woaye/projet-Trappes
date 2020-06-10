aee2acf3b08d17dcff8757e8a57ab3f8bb765375
AUTRET <autret@MacBook-Air-de-AUTRET.local>
2020-06-10 12:36:18 +0200

    change mode back

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenu sur GITHUB</title>
+</head>
+<body>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dolores ipsa consequatur repudiandae, autem sit voluptatibus numquam ipsum? Praesentium quibusdam earum amet, architecto cum minima. Excepturi, qui? Laborum, quis velit. </p>
+    
+</body>
+</html>
